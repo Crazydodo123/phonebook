572bb2e3dad499290e38ddec1a6bd908e3cfa39e
Crazydodo123 <peizheg@gmail.com>
2023-07-06 18:23:24 -0400

    part 3.10

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('request', function (req, res) {
   if (req.method === "POST") {
     return JSON.stringify(req.body)
   }
 })
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request'))
+
 
 let entries = [
   { 
