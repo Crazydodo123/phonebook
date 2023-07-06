ead9dfdeb9692a669eeac7720f0b6ea4fe0f9184
Crazydodo123 <peizheg@gmail.com>
2023-07-06 18:58:02 -0400

    fix heroku error

--- index.js
+++ index.js
@@ -95,7 +95,8 @@ app.post('/api/persons', (request, response) => {
   response.json(entry)
 })
 
-const PORT = 3001
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
