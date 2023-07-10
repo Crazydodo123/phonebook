57247a8147c8daf47c87781abf1a146fc474b24f
Crazydodo123 <peizheg@gmail.com>
2023-07-10 11:22:56 -0400

    part 3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://Peizhe:${password}@cluster0.xwjh17i.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const entrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Entry = new mongoose.model('Entry', entrySchema)
+
+if (process.argv.length === 3) {
+  console.log('phonebook:')
+  
+  Entry.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note.name, note.number)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const entry = new Entry({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
+  
+  entry.save().then(result => {
+    console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
