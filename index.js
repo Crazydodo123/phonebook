8fcba50bedad9ee4912d9ce8074ebc1f93f77048
Crazydodo123 <peizheg@gmail.com>
2023-07-10 13:52:16 -0400

    part 3.18

--- index.js
+++ index.js
@@ -20,41 +20,19 @@ morgan.token('request', function (req, res) {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request'))
 
 
-let entries = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-
 app.get('/info', (request, response) => {
-  response
-    .send(`
-      <p>Phonebook has info for ${entries.length} people</p>
-      <p>${Date()}</p>
-    `)
+  Entry.find({}).then(entries => {
+    response
+      .send(`
+        <p>Phonebook has info for ${entries.length} people</p>
+        <p>${Date()}</p>
+      `)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
-  Entry.find({}).then(entry => {
-    response.json(entry)
+  Entry.find({}).then(entries => {
+    response.json(entries)
   })
 })
 
