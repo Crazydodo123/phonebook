e83a805b579ca71211240856bf6d8e62e70d7eec
Crazydodo123 <peizheg@gmail.com>
2023-07-13 17:52:09 -0400

    part 3-19

--- index.js
+++ index.js
@@ -56,34 +56,29 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  
-  if (!body.name || !body.number) {
-    response.status(400).json({
-      error: 'name and/or number missing'
-    })
-  }
   
   const entry = new Entry({
     name: body.name,
     number: body.number,
   })
   
-  entry.save().then(savedEntry => {
-    response.json(savedEntry)
-  })
+  entry.save()
+    .then(savedEntry => {
+      response.json(savedEntry)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
   
-  const entry = {
-    name: body.name,
-    number: body.number
-  }
-  
-  Entry.findByIdAndUpdate(request.params.id, entry, { new:true })
+  Entry.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new:true, runValidators: true, context: 'query' }
+  )
     .then(updatedEntry => {
       response.json(updatedEntry)
     })
@@ -95,6 +90,8 @@ const errorHandler = (error, request, response, next) => {
   
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   
   next(error)
--- models/entry.js
+++ models/entry.js
@@ -14,7 +14,11 @@ mongoose.connect(url)
   })
   
 const entrySchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
   number: String,
 })
 
